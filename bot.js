779ec8d3d0f967521e89a0e35a54c1a1a9fb51a1
mustafaproducer <mustafaabdurahmonov7777@gmail.com>
2026-03-10 22:20:41 +0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -208,8 +208,17 @@ bot.on('callback_query', async (ctx) => {
     }
 });
 
+// --- RENDER UCHUN HTTP SERVER ---
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Bot is running!'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 // Botni ishga tushirish
-bot.launch().then(() => {
+bot.launch({ dropPendingUpdates: true }).then(() => {
     console.log("🚀 Premium Prompt boti ishga tushdi!");
 });
 
