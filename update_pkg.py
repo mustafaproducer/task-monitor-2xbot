c131bf9f52384f4cc38f6ae35d6fafebf828b78f
Mustafa <mustafa@mustafaproducer.local>
2026-03-22 11:45:09 +0500

    Fix: Add missing basic-auth dependency

--- update_pkg.py
+++ update_pkg.py
@@ -1,6 +1,6 @@
 import json
 with open('package.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
-data['dependencies']['ioredis'] = '^5.3.0'
+data['dependencies']['basic-auth'] = '^2.0.1'
 with open('package.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, indent=2)
